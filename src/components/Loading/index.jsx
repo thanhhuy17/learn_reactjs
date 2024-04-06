2999b99ac255b3a52edec51b2bc4612a30212d84
Huy Nguyen <huymau170819999@gmail.com>
2024-04-06 15:52:29 +0700

    Change Foder TodoList

---
+++ src/components/Loading/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import PropTypes from "prop-types";
+import "./styles.scss";
+
+Loading.propTypes = {};
+
+function Loading(props) {
+  return <div>Loadding</div>;
+}
+
+export default Loading;
---
+++ src/features/Todo/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+TodoFeatures.propTypes = {};
+
+function TodoFeatures(props) {
+  return <div></div>;
+}
+
+export default TodoFeatures;
