3bfe75f54a51ac0ba1b55d4ebcfc20ab5eb6e3d4
Huy Nguyen <huymau170819999@gmail.com>
2024-04-10 20:59:36 +0700

    Change State

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 // import "./App.css";
 // import TodoFeatures from "./features/Todo";
 import AlbumFeature from "./features/Album";
+import ColorBox from "./components/ColorBox";
+import Counter from "./components/Counter";
 function App() {
   return (
     <div className="App">
       {/* <TodoFeatures /> */}
       <AlbumFeature />
+      <ColorBox />
+      <Counter />
     </div>
   );
 }
---
+++ src/components/Counter/index.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+Counter.propTypes = {};
+
+function Counter() {
+  const [count, setCount] = useState(0);
+  return (
+    <div>
+      {count}
+      <button onClick={() => setCount((x) => x + 1)}>Increase</button>
+    </div>
+  );
+}
+
+export default Counter;
