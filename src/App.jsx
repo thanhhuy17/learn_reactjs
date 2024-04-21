838adb432ab0f346c60fa170fb4b18fce648bedd
Huy Nguyen <huymau170819999@gmail.com>
2024-04-21 12:29:46 +0700

    Change NAVLINK

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { Link, NavLink, Route, Routes } from "react-router-dom";
 import "./App.css";
 import TodoFutures from "./features/Todo";
 import AlbumFeature from "./features/Album";
@@ -8,7 +8,22 @@ function App() {
   return (
     <div className="App">
       HUY NGUYEN
+      <p>
+        <Link to="/http://localhost:5173}"> Chuyển đến Trang chủ </Link>
+      </p>
+      <p>
+        <Link to="/todo"> Chuyển đến Todo </Link>
+      </p>
+      <p>
+        <Link to="/album"> Chuyển đến Album </Link>
+      </p>
+      {/* NAVLink */}
+      <NavLink to="/todo" activeClassName="active-todo">
+        ActiveToDoNavLink
+      </NavLink>
+      <NavLink to="/Album">ActiveAlbumNavLink</NavLink>
       <Routes>
+        {/* <Route path="/MainPage" Component={"/http://localhost:5173}"} /> */}
         <Route path="/todo" Component={TodoFutures} />
         <Route path="/album" Component={AlbumFeature} />
       </Routes>
