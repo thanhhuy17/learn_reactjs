be83e960a231be4585af69acf3b6e64d174bd1c2
Huy Nguyen <huymau170819999@gmail.com>
2024-04-21 11:31:09 +0700

    Change Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import TodoFutures from "./features/Todo";
-// import AlbumFeature from "./features/Album";
+import AlbumFeature from "./features/Album";
 // import ColorBox from "./components/ColorBox";
 // import Counter from "./components/Counter";
 function App() {
   return (
     <div className="App">
-      <TodoFutures />
-      {/* <AlbumFeature />
-      <ColorBox />
-      <Counter /> */}
+      HUY NGUYEN
+      <Routes>
+        <Route path="/todo" Component={TodoFutures} />
+        <Route path="/album" Component={AlbumFeature} />
+      </Routes>
     </div>
   );
 }
